a42b24656b778a0e5e4c7190d80f841349556713
Davor Grinfelder <grinfelderdavor@gmail.com>
2025-07-04 12:21:39 +0200

    initial commit

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import authRoute from './routes/auth.js'
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB");
+    } catch (error) {
+        throw error;
+    }
+}
+
+//middlewares
+app.use("/auth", authRoute);
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+})
+
+app.listen(3000, () => {
+    connect();
+    console.log('Connected to backend!');
+})
---
+++ api/routes/auth.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    res.send("Hello, this is register endpoint");
+})
+
+router.post("/login", (req, res) => {
+    res.send("Hello, this is login endpoint");
+})
+
+router.get("/user", (req, res) => {
+    res.send("get user");
+})
+
+router.post("/logout", (req, res) => {
+    res.send("logout");
+})
+
+export default router;
