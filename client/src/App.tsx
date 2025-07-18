fc7214aa9b231c48412baad25e1c997988741a43
Davor Grinfelder <grinfelderdavor@gmail.com>
2025-07-18 15:10:24 +0200

    reusable responsive auth form with spinner and toast notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { ToastContainer } from 'react-toastify';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
 
 function App() {
-	return <Login />;
+	return (
+		<Router>
+			<ToastContainer position='top-center' autoClose={3000} />
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
